13d0836fe00a5149b00c69c75c69b1313837346a
Faisal Memon <faisalmemonuk@yahoo.com>
2012-06-29 15:46:19 +0100

    add hello world gae skeleton

---
+++ server/SprintOverflow/src/com/pcc/SprintOverflow/SprintOverflowServlet.java
@@ -0,0 +1,13 @@
+package com.pcc.SprintOverflow;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SprintOverflowServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
