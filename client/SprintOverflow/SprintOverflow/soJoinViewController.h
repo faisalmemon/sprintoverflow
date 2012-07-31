235c606329274451da27fa6725e98000a5b321f1
Faisal Memon <faisalmemonuk@yahoo.com>
2012-07-31 19:02:35 +0100

    Renaming view controller first to welcome

---
+++ client/SprintOverflow/SprintOverflow/soJoinViewController.h
@@ -0,0 +1,13 @@
+//
+//  soJoinViewController.h
+//  SprintOverflow
+//
+//  Created by Faisal Memon on 31/07/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface soJoinViewController : UIViewController
+
+@end
